35cf67db64b6441b18c2e21be522a2506ebbd93d
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-04-13 20:49:38 +0300

    README and React app

---
+++ apps/react-rspack/src/app/app.tsx
@@ -0,0 +1,13 @@
+import styles from './app.module.css';
+
+export function App() {
+  return (
+    <>
+      <div className={styles['katerina']}></div>
+      <div className={styles['anastasis']}></div>
+      <div className={styles['manmu']}></div>
+    </>
+  );
+}
+
+export default App;
